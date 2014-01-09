f96e85705568883ed1a3de2bd1bb3593b6c3fc34
Mike McNeil <mike@balderdash.co>
2014-01-09 01:55:47 -0600

    Create hooks.js

---
+++ plugins/hooks.js
@@ -0,0 +1,25 @@
+
+/**
+ * Known hooks for Sails.js.
+ *
+ * The custom hooks API has not settled yet-- custom hooks are still experimental.
+ * Please read: https://github.com/balderdashy/sails/tree/v0.10/lib/hooks#custom-hooks--plugins
+ */
+module.exports = {
+
+  //
+  // Core hooks
+  // 
+  
+  // TODO
+  
+  
+  
+  
+  //
+  // Community hooks
+  //
+  
+  // TODO
+  
+};
