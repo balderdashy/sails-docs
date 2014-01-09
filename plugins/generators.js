8b2e09df9d7b51cc4b96910b8449ca298b5fd01d
Mike McNeil <mike@balderdash.co>
2014-01-09 01:57:03 -0600

    Create generators.js

---
+++ plugins/generators.js
@@ -0,0 +1,24 @@
+/**
+ * Known generators for Sails.js.
+ *
+ * The `generators` API has not settled yet-- it is still experimental.
+ * Please read: https://github.com/balderdashy/sails/blob/master/CONTRIBUTING.md#authoring-a-new-generator
+ */
+module.exports = {
+
+  //
+  // Core generators
+  // 
+  
+  // TODO
+  
+  
+  
+  
+  //
+  // Community generators
+  //
+  
+  // TODO
+  
+};
