05c6245dc52f70ffc38f1b551c159f87bb93ae89
Mike McNeil <mike@balderdash.co>
2014-01-09 01:57:17 -0600

    Update hooks.js

--- plugins/hooks.js
+++ plugins/hooks.js
@@ -1,8 +1,7 @@
-
 /**
  * Known hooks for Sails.js.
  *
- * The custom hooks API has not settled yet-- custom hooks are still experimental.
+ * The hooks API has not settled yet-- it is still experimental.
  * Please read: https://github.com/balderdashy/sails/tree/v0.10/lib/hooks#custom-hooks--plugins
  */
 module.exports = {
